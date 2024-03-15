e8079e3a1f3c6d2ff6dab54496f69801d833b13b
Esmée Zootjes <esmeezootjes@gmail.com>
2024-03-15 12:02:52 +0100

    setting up the startcode + the game class

--- js/game.js
+++ js/game.js
@@ -1,3 +1,42 @@
 class Game {
-  // code to be added
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = null;
+    this.height = 600;
+    this.width = 500;
+    this.obstacles = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId;
+    this.gameLoopFrequency = Math.round(1000 / 60); // about 60frames per second screen refresh
+  }
+
+  start() {
+    // set the height + width of the game screen
+    this.gameScreen.height = `${this.height}px`;
+    this.gameScreen.width = `${this.width}px`;
+
+    // change the visibility of the start screen
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+
+  gameLoop() {
+    console.log("in the game loop");
+    this.update();
+    if (this.gameIsOver) {
+      clearInterval(this.gameIntervalId);
+    }
+  }
+
+  update() {
+    console.log(`updating game`);
+  }
 }
